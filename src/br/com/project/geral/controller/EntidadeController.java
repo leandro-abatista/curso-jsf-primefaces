932df75952955f784d6b5e7fc8e705b1d20a0217
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-17 09:36:18 -0300

    Corrigindo erro de cast d elist para objeto entidade

--- src/br/com/project/geral/controller/EntidadeController.java
+++ src/br/com/project/geral/controller/EntidadeController.java
@@ -1,6 +1,7 @@
 package br.com.project.geral.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,8 +19,18 @@ public class EntidadeController extends ImplementacaoCrud<Entidade> implements I
 	@Autowired//injeção de dependência
 	private ServiceEntidade serviceEntidade;
 	
+	@SuppressWarnings("unchecked")
 	public Entidade findUserLogado(String usuarioLogado) throws Exception {
-		return super.findUniqueByProperty(Entidade.class, usuarioLogado, "ent_login", " and entity.ent_inativo is false");
+		
+		List<Entidade> entidades = (List<Entidade>) 
+				super.findUniqueByProperty(Entidade.class, usuarioLogado, "ent_login", " and entity.ent_inativo is false");
+		
+		if (entidades != null && !entidades.isEmpty()) {
+			return entidades.get(0);//retorna o primeiro objeto. Na lista começa com 0 a primeira posição.
+		}
+		
+		return null;
+		
 	}
 	
 	public Date getUltimoAcessoEntidadeLogada(String login) {
