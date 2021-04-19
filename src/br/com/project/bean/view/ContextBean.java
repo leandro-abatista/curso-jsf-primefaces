e585bd4aef5e4e980d43a04849f5fa32339bed6e
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-19 17:59:10 -0300

    Criando os componentes com comosite parte - 2

--- src/br/com/project/bean/view/ContextBean.java
+++ src/br/com/project/bean/view/ContextBean.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -82,6 +83,20 @@ public class ContextBean implements Serializable {
 			}
 		}
 		return entidade;
+	}
+	
+	public boolean possuiAcesso(String... acessos) {
+		
+		for (String acesso : acessos) {
+			
+			for(GrantedAuthority authority : getAuthentication().getAuthorities()) {
+				
+				if (authority.getAuthority().trim().equals(acesso.trim())) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 
