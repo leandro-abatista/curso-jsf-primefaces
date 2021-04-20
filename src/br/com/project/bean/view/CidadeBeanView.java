875b794d4d47578e2e34267f04cde513f318e052
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-19 22:58:18 -0300

    Criando os componentes com composite parte - 5

--- src/br/com/project/bean/view/CidadeBeanView.java
+++ src/br/com/project/bean/view/CidadeBeanView.java
@@ -73,16 +73,17 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 	
 	@Override
 	public String editar() throws Exception {
-		
+		list.clear();//limpa a lista
 		return url;
 	}
 	
 	@Override
 	public void excluir() throws Exception {
+		objetoSelecionado = (Cidade) cidadeController.getSession().get(Cidade.class, objetoSelecionado.getCid_codigo());
 		cidadeController.delete(objetoSelecionado);
 		list.remove(objetoSelecionado);
 		novo();
-		
+		sucesso();
 	}
 	
 	public Cidade getObjetoSelecionado() {
