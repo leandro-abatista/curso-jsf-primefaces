d9da3c941ad6aec370c3fc85d6dc07146f744acf
Leandro Amorim <leandroamorimpb@gmail.com>
2021-04-27 11:05:00 -0300

    Criando a tela de pesquisa parte 3

--- src/br/com/project/bean/view/BeanManagedViewAbstract.java
+++ src/br/com/project/bean/view/BeanManagedViewAbstract.java
@@ -1,8 +1,19 @@
 package br.com.project.bean.view;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
 import org.springframework.stereotype.Component;
 
 import br.com.framework.interfaces.crud.InterfaceCrud;
+import br.com.project.annotation.IdentificaCampoPesquisa;
 import br.com.project.bean.geral.ObjetoCampoConsulta;
 import br.com.project.report.util.BeanReportView;
 
@@ -16,6 +27,8 @@ public abstract class BeanManagedViewAbstract extends BeanReportView {
 	protected abstract InterfaceCrud<?> getController();
 	
 	public ObjetoCampoConsulta objetoCampoConsultaSelecionado;
+	
+	public List<SelectItem> listaSelectItemsCampoPesquisa;
 
 	public ObjetoCampoConsulta getObjetoCampoConsultaSelecionado() {
 		return objetoCampoConsultaSelecionado;
@@ -24,5 +37,53 @@ public abstract class BeanManagedViewAbstract extends BeanReportView {
 	public void setObjetoCampoConsultaSelecionado(ObjetoCampoConsulta objetoCampoConsultaSelecionado) {
 		this.objetoCampoConsultaSelecionado = objetoCampoConsultaSelecionado;
 	}
+	
+	public List<SelectItem> getListaSelectItemsCampoPesquisa() {
+		
+		listaSelectItemsCampoPesquisa = new ArrayList<SelectItem>();
+		
+		List<ObjetoCampoConsulta> listTemporaria = new ArrayList<ObjetoCampoConsulta>();
+		
+		for (Field field : getClassImplement().getDeclaredFields()) {
+			
+			if (field.isAnnotationPresent(IdentificaCampoPesquisa.class)) {//identifica se existe essa anotação na classe de cidade
+				
+				String descricaoCampo = field.getAnnotation(IdentificaCampoPesquisa.class).descricaoCampo();
+				String descricaoCampoPesquisa = field.getAnnotation(IdentificaCampoPesquisa.class).campoConsulta();
+				int isPrincipal = field.getAnnotation(IdentificaCampoPesquisa.class).principal();	
+				
+				ObjetoCampoConsulta objetoCampoConsulta = new ObjetoCampoConsulta();
+				objetoCampoConsulta.setDescricao(descricaoCampo);
+				objetoCampoConsulta.setCampoBanco(descricaoCampoPesquisa);
+				objetoCampoConsulta.setTipoClass(field.getType().getCanonicalName());
+				objetoCampoConsulta.setPrincipal(isPrincipal);
+				
+				listTemporaria.add(objetoCampoConsulta);
+			}
+		}
+		
+		orderReverse(listTemporaria);
+		
+		for (ObjetoCampoConsulta objetoCampoConsulta : listTemporaria) {
+			//transforma a lista de objetoCampoConsulta em um selectItem
+			listaSelectItemsCampoPesquisa.add(new SelectItem(objetoCampoConsulta));
+		}
+		
+		return listaSelectItemsCampoPesquisa;
+	}
+
+	private void orderReverse(List<ObjetoCampoConsulta> listTemporaria) {
+		Collections.sort(listTemporaria, new Comparator<ObjetoCampoConsulta>() {
+
+			@Override
+			public int compare(ObjetoCampoConsulta objeto1, ObjetoCampoConsulta objeto2) {
+				
+				return objeto1.getPrincipal().compareTo(objeto2.getPrincipal());
+			}
+		});
+		
+	}
+	
+	
 	
 }
--- src/br/com/project/converters/ObjetoCampoConsultaConverter.java
+++ src/br/com/project/converters/ObjetoCampoConsultaConverter.java
@@ -13,12 +13,27 @@ import br.com.project.bean.geral.ObjetoCampoConsulta;
 public class ObjetoCampoConsultaConverter implements Converter, Serializable {
 
 	@Override
-	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+	public Object getAsObject(FacesContext arg0, UIComponent arg1, String value) {
+		
+		if (value != null && !value.isEmpty()) {//aqui retorna para o lado do servidor
+			String[] valores = value.split("\\*");
+			ObjetoCampoConsulta objetoCampoConsulta = new ObjetoCampoConsulta();
+			objetoCampoConsulta.setCampoBanco(valores[0]);
+			objetoCampoConsulta.setTipoClass(valores[1]);
+			
+			return objetoCampoConsulta;
+		}
 		return null;
 	}
 
 	@Override
-	public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+	public String getAsString(FacesContext arg0, UIComponent arg1, Object objeto) {
+		
+		if (objeto != null) {
+			ObjetoCampoConsulta c = (ObjetoCampoConsulta) objeto;
+			
+			return c.getCampoBanco() + "*" + c.getTipoClass();//aqui retorna para o combo
+		}
 		return null;
 	}
 
